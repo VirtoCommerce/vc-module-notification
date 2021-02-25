242774a1cd83ebfc232e60bc6aaf63fc12bd1b1f
Aleksandr Andreev <alivemenone@gmail.com>
2021-02-25 07:51:41 +0300

    Add description Reformat

--- src/VirtoCommerce.NotificationsModule.Core/Types/VerificationEmailNotification.cs
+++ src/VirtoCommerce.NotificationsModule.Core/Types/VerificationEmailNotification.cs
@@ -2,21 +2,20 @@ using VirtoCommerce.NotificationsModule.Core.Model;
 
 namespace VirtoCommerce.NotificationsModule.Core.Types
 {
+    /// <summary>
+    /// Account email address verification notification
+    /// The sent link will change verification status for an account email address
+    /// </summary>
     public class VerificationEmailNotification : EmailNotification
     {
         public VerificationEmailNotification() : base(nameof(VerificationEmailNotification))
         {
-            //for backward compatibility v.2
-            Alias = "EmailVerificationNotification";
         }
 
         public VerificationEmailNotification(string type) : base(type)
         {
-
         }
 
         public string Url { get; set; }
-
     }
-
 }
