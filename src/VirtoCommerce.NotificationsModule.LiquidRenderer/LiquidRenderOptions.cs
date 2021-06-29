cc19a158b67323d9f78309e49585634d9079bc09
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-06-29 15:16:43 +0200

    PT-2082 increased loop limit in Liquid renderer (#86)

--- src/VirtoCommerce.NotificationsModule.LiquidRenderer/LiquidRenderOptions.cs
+++ src/VirtoCommerce.NotificationsModule.LiquidRenderer/LiquidRenderOptions.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using VirtoCommerce.NotificationsModule.Core;
 
 namespace VirtoCommerce.NotificationsModule.LiquidRenderer
 {
     public class LiquidRenderOptions
     {
         public HashSet<Type> CustomFilterTypes { get; set; } = new HashSet<Type>();
+
+        public int LoopLimit { get; set; } = ModuleConstants.DefaultLiquidRendererLoopLimit;
     }
 }
