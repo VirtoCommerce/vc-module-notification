a8a3b6ef52a622713b9766e627b3c1acddf9aa6c
Leysan Nigmatullina <86958548+einlied753@users.noreply.github.com>
2022-06-30 15:54:23 +0300

    PT-8177: Fix sms notification message length (#98)

---
+++ src/VirtoCommerce.NotificationsModule.Data/Migrations/20220630084908_FixSmsNotificationMessageLength.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.NotificationsModule.Data.Migrations
+{
+    public partial class FixSmsNotificationMessageLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "NotificationTemplate",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1600)",
+                oldMaxLength: 1600,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "NotificationMessage",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1600)",
+                oldMaxLength: 1600,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "NotificationTemplate",
+                type: "nvarchar(1600)",
+                maxLength: 1600,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "NotificationMessage",
+                type: "nvarchar(1600)",
+                maxLength: 1600,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
