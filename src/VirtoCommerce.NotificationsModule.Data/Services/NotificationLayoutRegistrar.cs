95c3cef6a06e296ab87295278ea539ae70d161d5
Jounik <epo@virtoway.com>
2023-04-12 20:23:28 +0300

    fixing the access modifier

--- src/VirtoCommerce.NotificationsModule.Data/Services/NotificationLayoutRegistrar.cs
+++ src/VirtoCommerce.NotificationsModule.Data/Services/NotificationLayoutRegistrar.cs
@@ -66,7 +66,7 @@ namespace VirtoCommerce.NotificationsModule.Data.Services
             return result;
         }
 
-        private void SaveChanges()
+        public void SaveChanges()
         {
             var criteria = new NotificationLayoutSearchCriteria
             {
