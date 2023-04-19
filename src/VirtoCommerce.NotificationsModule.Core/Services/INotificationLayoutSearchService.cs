5b810f145a05c98085c1cac9b8adb047d4a7dcbf
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2023-04-19 12:33:32 +0300

    Fix service interfaces inheritance

--- src/VirtoCommerce.NotificationsModule.Core/Services/INotificationLayoutSearchService.cs
+++ src/VirtoCommerce.NotificationsModule.Core/Services/INotificationLayoutSearchService.cs
@@ -1,9 +1,13 @@
+using VirtoCommerce.NotificationsModule.Core.Model;
+using VirtoCommerce.NotificationsModule.Core.Model.Search;
+using VirtoCommerce.Platform.Core.GenericCrud;
+
 namespace VirtoCommerce.NotificationsModule.Core.Services
 {
     /// <summary>
     /// The service is for to make a query to Database and get a list of notification layouts
     /// </summary>
-    public interface INotificationLayoutSearchService
+    public interface INotificationLayoutSearchService : ISearchService<NotificationLayoutSearchCriteria, NotificationLayoutSearchResult, NotificationLayout>
     {
     }
 }
