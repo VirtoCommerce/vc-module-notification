2bb615bf248e91efa619547e46bb1cbc0b05bc3d
Oleg Zhuk <zhukoo@gmail.com>
2023-04-24 10:37:49 +0200

    Update Sample webpack.

--- samples/VirtoCommerce.NotificationsSampleModule.Web/webpack.config.js
+++ samples/VirtoCommerce.NotificationsSampleModule.Web/webpack.config.js
@@ -1,19 +1,53 @@
-const glob = require("glob");
-const path = require("path");
-const webpack = require("webpack");
+const namespace = 'VirtoCommerce.Notifications.Sample'
 
-module.exports = [
-    {
-        entry: glob.sync('./Scripts/**/*.js', { nosort: true }),
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'app.js'
-        },
+const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const rootPath = path.resolve(__dirname, 'dist');
+
+function getEntryPoints() {
+    const result = [
+        ...glob.sync('./Scripts/**/*.js', { nosort: true }),
+        ...glob.sync('./Content/**/*.css', { nosort: true }),
+    ];
+
+    return result;
+}
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: getEntryPoints(),
         devtool: false,
+        output: {
+            path: rootPath,
+            filename: 'app.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                }
+            ]
+        },
         plugins: [
-            new webpack.SourceMapDevToolPlugin({
-                namespace: 'VirtoCommerce.Notifications'
-            })          
+            new CleanWebpackPlugin(),
+            isProduction ?
+                new webpack.SourceMapDevToolPlugin({
+                    namespace: namespace,
+                    filename: '[file].map[query]'
+                }) :
+                new webpack.SourceMapDevToolPlugin({
+                    namespace: namespace
+                }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+            })
         ]
-    }
-];
+    };
+};
