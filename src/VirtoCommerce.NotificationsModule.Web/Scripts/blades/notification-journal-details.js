7a544679fc75ac5dc468a3c9b2e7ec8313d47bee
Evgeny Okhrimenko <evgokhrimenko@gmail.com>
2025-03-07 10:20:49 +0200

    VCST-2663: Add email message body preview (#176)

--- src/VirtoCommerce.NotificationsModule.Web/Scripts/blades/notification-journal-details.js
+++ src/VirtoCommerce.NotificationsModule.Web/Scripts/blades/notification-journal-details.js
@@ -1,15 +1,43 @@
 angular.module('virtoCommerce.notificationsModule')
-    .controller('virtoCommerce.notificationsModule.notificationJournalDetailsController', ['$scope', 'virtoCommerce.notificationsModule.notificationsModuleApi', 'platformWebApp.bladeNavigationService', function ($scope, notifications, bladeNavigationService) {
-	var blade = $scope.blade;
+    .controller('virtoCommerce.notificationsModule.notificationJournalDetailsController',
+        ['$scope', 'virtoCommerce.notificationsModule.notificationsModuleApi', 'platformWebApp.bladeNavigationService',
+            function ($scope, notifications, bladeNavigationService) {
+                var blade = $scope.blade;
+                blade.readPermission = 'notifications:read';
 
-	blade.initialize = function () {
-		notifications.getNotificationJournalDetails({ id: blade.currentNotificationId }, function (data) {
-			blade.currentEntity = data;
-			blade.isLoading = false;
-		}, function (error) {
-			bladeNavigationService.setError('Error ' + error.status, $scope.blade);
-		});
-	}
+                blade.initialize = function () {
+                    notifications.getNotificationJournalDetails({ id: blade.currentNotificationId }, function (data) {
+                        blade.currentEntity = data;
+                        blade.isLoading = false;
+                    }, function (error) {
+                        bladeNavigationService.setError('Error ' + error.status, $scope.blade);
+                    });
+                }
 
-	blade.initialize();
-}]);
+                blade.openEmailHtmlBlade = function () {
+                    var newBlade = {
+                        id: 'emailHtmlContentBlade',
+                        controller: 'virtoCommerce.notificationsModule.notificationJournalDetailsContentController',
+                        template: 'Modules/$(VirtoCommerce.Notifications)/Scripts/blades/notification-journal-details-content.html',
+                        currentEntity: blade.currentEntity,
+                    };
+
+                    bladeNavigationService.showBlade(newBlade, blade);
+                }
+
+                if (blade.currentEntity.kind === 'EmailNotification') {
+                    blade.toolbarCommands = [
+                        {
+                            name: 'platform.commands.preview',
+                            icon: 'fas fa-eye',
+                            executeMethod: blade.openEmailHtmlBlade,
+                            canExecuteMethod: function () { return true; },
+                            permission: blade.readPermission,
+                        }
+                    ];
+                }
+
+                blade.initialize();
+            }
+        ]
+    );
